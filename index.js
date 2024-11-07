0c05f4a14abdc82fcff1099d08451faf131b296b
Vladislava <stdemidovez@gmail.com>
2024-11-07 20:31:01 +0300

    chore: add prettier and eslint

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-import express from 'express'
-import cors from "cors"
+import express from 'express';
+import cors from 'cors';
 
 const PORT = 5000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 app.post('/', (req, res) => {
-    console.log(req.body)
-    res.status(200).json("Server work")
-})
+  console.log(req.body);
+  res.status(200).json('Server work');
+});
 
 app.listen(PORT, () => {
-    console.log("SERVER STARTED ON PORT: " + PORT)
-})
-
+  console.log('SERVER STARTED ON PORT: ' + PORT);
+});
